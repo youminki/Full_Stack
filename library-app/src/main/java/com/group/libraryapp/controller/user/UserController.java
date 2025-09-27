1dd33848c1bda3f56d707ee235bed69ebe21acbc
youminki <dbalsrl7648@gmail.com>
2025-09-27 17:10:49 +0900

    Add UserResponse DTO and Fruit domain class
    Introduced the UserResponse DTO for user API responses and added the Fruit domain class. Updated UserController to provide a GET endpoint returning a list of UserResponse objects. Also added getter methods to the User class for name and age.

--- library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,7 +1,10 @@
 package com.group.libraryapp.controller.user;
 
+import com.group.libraryapp.domain.user.Fruit;
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,15 @@ public class UserController {
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
         users.add(new User(request.getName(), request.getAge()));
+    }
+
+    @GetMapping("/user")
+    public List<UserResponse> getUser() {
+        List<UserResponse> responses = new ArrayList<>();
+        for(int i=0; i<users.size(); i++) {
+            responses.add(new UserResponse(i+1, users.get(i)));
+        }
+        return responses;
     }
 
 }
--- library-app/src/main/java/com/group/libraryapp/domain/user/User.java
+++ library-app/src/main/java/com/group/libraryapp/domain/user/User.java
@@ -12,5 +12,13 @@ public class User {
         this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
 
 }
